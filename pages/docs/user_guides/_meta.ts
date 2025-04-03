81213fd54e92a3a9b0183221e25217b44c4aa2e7
EricEcosystem <eric@ecosystem.ai>
2025-04-03 08:55:37 +0200

    Cleanup exploration and virtual variables

--- pages/docs/user_guides/_meta.ts
+++ pages/docs/user_guides/_meta.ts
@@ -3,4 +3,6 @@ export default {
   recommender: 'Recommender',
   dynamic: 'Dynamic Recommender',
   runtime_plugin_development: 'Runtime Plugin Development',
+  epsilon_exploration: 'Exploration using epsilon',
+  virtual_variables: 'Virtual Variables'
 }
