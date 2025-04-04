430114f3af632293480a92527edaef2ed9b9ccb1
ecosystem.Ai <jay@ecosystem.ai>
2025-04-04 11:23:44 +0200

    network selector

--- pages/docs/user_guides/_meta.ts
+++ pages/docs/user_guides/_meta.ts
@@ -2,7 +2,12 @@ export default {
   index: 'Intro',
   recommender: 'Recommender',
   dynamic: 'Dynamic Recommender',
+  contributing: {
+    "title": "Runtime Configurations",
+    type: 'separator',
+  },
   runtime_plugin_development: 'Runtime Plugin Development',
-  epsilon_exploration: 'Exploration using epsilon',
-  virtual_variables: 'Virtual Variables'
+  epsilon_exploration: 'Runtime Exploration using epsilon',
+  virtual_variables: 'Runtime Virtual Variables',
+  network: 'Runtime Network Selector',
 }
